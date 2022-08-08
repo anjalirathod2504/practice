884a12240268100da16cada3a115a4c74b8883eb
anjalirathod2504 <95164620+anjalirathod2504@users.noreply.github.com>
2022-08-08 11:48:55 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -12,6 +12,6 @@ for number in range (2,n + 1):
     for i in range (2, number):
         if (number % i) == 0:
             break
-        else:
-            print (number)  
+    else:
+        print (number)  
 
